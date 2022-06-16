709f021eb30dc934a8caa4b5a5470fb984b82d4e
Filip-231 <filip.szmid@gmail.com>
2022-06-16 21:14:41 +0200

    PyCharm auto-commit

--- templates/django/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings.py
+++ templates/django/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--vdr11aot*1-s_vrj=g3#+n5ccd7j$=^llb*_%g-a1vjh+2t4#"
+SECRET_KEY = "django-insecure--vdr11aot*1-s_vrj=g3#+n5ccd7j$=^llb*_%g-a1vjh+2t4#"  # dodgy: disable=secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
