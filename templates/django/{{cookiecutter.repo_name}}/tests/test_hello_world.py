d18d0f3fc32b7587c3b62915955527dcb7eba2a5
Filip-231 <filip.szmid@gmail.com>
2022-06-16 21:32:31 +0200

    PyCharm auto-commit

--- templates/django/{{cookiecutter.repo_name}}/tests/test_hello_world.py
+++ templates/django/{{cookiecutter.repo_name}}/tests/test_hello_world.py
@@ -1,6 +1,7 @@
+"""Tests for django module."""
+
 import pytest
 
 
-def test_hello_world():
-    print("test done")
-    assert 1 == 1
+def test_hello_world()->None:
+    """Blank dummy test."""
--- templates/django/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
+++
@@ -1,1 +0,0 @@
-# pylint: disable=django-not-configured
