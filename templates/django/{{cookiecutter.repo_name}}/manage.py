a2de5ea329e720c39d19ee0ec6f0a952cd7b4e4d
Filip-231 <filip.szmid@gmail.com>
2022-07-10 16:32:14 +0200

    auto

--- templates/django/{{cookiecutter.repo_name}}/manage.py
+++ templates/django/{{cookiecutter.repo_name}}/manage.py
@@ -12,7 +12,7 @@ def main() -> None:
     try:
         from django.core.management import (
             execute_from_command_line,
-        )
+        )  # type: ignore[import]
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
