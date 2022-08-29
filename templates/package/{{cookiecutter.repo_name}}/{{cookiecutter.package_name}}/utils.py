2b3652a63b8d27198985681b96defde0c1e5f53c
Filip-231 <filip.szmid@gmail.com>
2022-08-29 19:13:38 +0200

    feat: autocommit

--- templates/package/{{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/utils.py
+++ templates/package/{{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/utils.py
@@ -1,2 +1,9 @@
-def test_method(x: int) -> int:
-    return x * x
+"""This is test method module."""
+
+
+def test_method(value: int) -> int:
+    """This is test method
+
+    :param value: Example number.
+    """
+    return value * value
